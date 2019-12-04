fd43d0cb8ae7408204c68393133f26e99c869bf4
LilAlvino <trousst13@gmail.com>
2019-12-04 19:34:11 +0100

    Choice question zit erin

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,10 +7,16 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Question q1 = new Question() { Vraag = "Op welke dag vieren we prinsjesdag?", Antwoord = "Dinsdag"};
-    
-            ChoiceQuestion q2 = new ChoiceQuestion();
+            Question q1 = new Question() { Vraag = "Op welke dag vieren we prinsjesdag?", Antwoord = "Dinsdag" };
+
+            ChoiceQuestion q2 = new ChoiceQuestion() { Vraag = "Wanneer is het eerste kerstdag?"};
+            q2.addChoice("24 december", false);
+            q2.addChoice("25 december", true);
+            q2.addChoice("26 december",false);
+            q2.addChoice(" 5 december", false);
+
             presentQuestion(q1);
+            presentQuestion(q2);
 
         }
 
@@ -49,8 +55,22 @@ namespace ConsoleApp5
 
     public class ChoiceQuestion : Question
     {
-        //private List<string> choices = new List<string>;
+        public List<String> choices = new List<string>;
 
+        public void addChoice(string possibleAnswer, Boolean measure)
+        {
+            choices.Add(possibleAnswer);
+            if (measure == true)
+            {
+                Antwoord = possibleAnswer;
+            }
+        }
         
+        public new void display()
+        {
+            Console.WriteLine(Vraag);
+
+           
+        }
     }
 }
